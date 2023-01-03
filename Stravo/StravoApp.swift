a9ae0404710bea8fdec9968b85a229ca923f4659
Bob Voorneveld <info@bobvoorneveld.nl>
2023-01-03 10:40:47 +0100

    Initial Commit

---
+++ Stravo/StravoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StravoApp.swift
+//  Stravo
+//
+//  Created by Bob Voorneveld on 03/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct StravoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
